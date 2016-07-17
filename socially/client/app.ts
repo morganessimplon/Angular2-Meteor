0b8cf741d835ae075f8d57cc107a6ba637518c84
morganessimplon <morganessimplon@gmail.com>
2016-07-17 19:25:19 +0200

    Debut creation app

---
+++ socially/client/app.ts
@@ -0,0 +1,37 @@
+import 'reflect-metadata';
+import { Component } from '@angular/core';
+import { bootstrap} from '@angular/platform-browser-dynamic';
+
+import template from './app.html';
+
+@Component({
+    selector: 'app',
+    template
+})
+class Socially {
+
+    parties: Array<Object>;
+
+
+    constructor() {
+        this.parties = [
+            {
+                'name': 'Dubstep-Free Zone',
+                'description': 'Can we please just for an evening not listen to dubstep.',
+                'location': 'Palo Alto'
+            },
+            {
+                'name': 'All dubstep all the time',
+                'description': 'Get it on!',
+                'location': 'Palo Alto'
+            },
+            {
+                'name': 'Savage lounging',
+                'description': 'Leisure suit required. And only fiercest manners.',
+                'location': 'San Francisco'
+            }
+        ];
+    }
+}
+
+bootstrap(Socially);
---
+++ socially/collections/parties.ts
@@ -0,0 +1,6 @@
+import { Mongo } from 'meteor/mongo';
+
+export let Parties = new Mongo.Collection('parties');
+
+
+
