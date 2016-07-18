b98540a5895a4e7ee768abec91056fa394055e6b
morganessimplon <morganessimplon@gmail.com>
2016-07-18 11:45:52 +0200

    Step 4 done - Add/Remove objects & deal with events

--- meteor-angular2.0-socially-step_03/client/app.ts
+++ meteor-angular2.0-socially-step_03/client/app.ts
@@ -3,18 +3,25 @@ import { Component } from '@angular/core';
 import { bootstrap } from 'angular2-meteor-auto-bootstrap';
 import { Parties }   from '../collections/parties';
 import { Mongo }     from 'meteor/mongo';
-
+import { PartiesForm } from '../collections/imports/parties-form/parties-form';
+import { provideRouter, RouterConfig, ROUTER_DIRECTIVES } from '@angular/router';
+ 
 import template from './app.html';
 
 @Component({
   selector: 'app',
-  template
+  template,
+  directives: [PartiesForm, ROUTER_DIRECTIVES]
 })
 class Socially {
   parties: Mongo.Cursor<Object>;
 
   constructor () {
     this.parties = Parties.find();
+  }
+
+  removeParty(party) {
+    Parties.remove(party._id);
   }
 }
 
